13d642bf68280d014fcc07fb24288359b99341ed
misterEarly <afi.lovisfrueh@gmail.com>
2021-06-15 00:36:59 +0200

    finished and debugged colision, game now working

--- snake_p5js/snake_p5js.js
+++ snake_p5js/snake_p5js.js
@@ -2,10 +2,39 @@ let tiles;
 let w;
 let pos;
 let tail;
-let len;
 let u, d, l, r;
 let dir;
 let fruit;
+
+function reset() {
+  keyCode = 0;
+  tail = [];
+  pos = createVector(7, 7);
+
+  tail.push(pos);
+  for (i = 1; i < 5; i++) {
+    tail.push(createVector(pos.x, pos.y + 1 * i));
+  }
+  dir = u;
+  fruit = createVector(floor(random(tiles)), floor(random(tiles)));
+}
+
+function keyPressed() {
+
+  if (keyIsDown(LEFT_ARROW)) {
+    dir = l;
+    keyCode = 0;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    dir = r;
+    keyCode = 0;
+  } else if (keyIsDown(UP_ARROW)) {
+    dir = u;
+    keyCode = 0;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    dir = d;
+    keyCode = 0;
+  }
+}
 
 function setup() {
   frameRate(5);
@@ -13,37 +42,12 @@ function setup() {
   w = 40;
   createCanvas(tiles * w + 1, tiles * w + 1);
 
-
   u = createVector(0, -1);
   d = createVector(0, 1);
   l = createVector(-1, 0);
   r = createVector(1, 0);
 
   reset();
-}
-
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    dir = l;
-  } else if (keyCode === RIGHT_ARROW) {
-    dir = r;
-  } else if (keyCode === UP_ARROW) {
-    dir = u;
-  } else if (keyCode === DOWN_ARROW) {
-    dir = d;
-  }
-}
-
-function reset() {
-  tail = [];
-  pos = createVector(7, 7);
-  len = 5;
-  tail.push(pos);
-  for (i = 1; i < len; i++) {
-    tail.push(createVector(pos.x, pos.y+(1*i)));
-  }
-  dir = u;
-  fruit = createVector(floor(random(tiles)), floor(random(tiles)));
 }
 
 function draw() {
@@ -63,18 +67,16 @@ function draw() {
   //console.log(pos.y)
 
   if (!(tail[0].x == fruit.x && tail[0].y == fruit.y)) {
-    tail.splice(tail.length-1, 1);
+    tail.splice(tail.length - 1, 1);
   } else {
     fruit = createVector(floor(random(tiles)), floor(random(tiles)));
   }
-  for (i = 1; i<tail.length; i++) {
-    if (tail[0].x==tail[i].x && tail[0].y==tail[i].y) {
-      console.log('dead');
+  for (i = 1; i < tail.length; i++) {
+    if (tail[0].x == tail[i].x && tail[0].y == tail[i].y) {
+      console.log("dead");
       reset();
     }
   }
-
-
 
   if (tail[0].x < 0) {
     tail[0].x = tiles - 1;
@@ -86,12 +88,11 @@ function draw() {
     tail[0].y = 0;
   }
 
-
   fill(80, 255, 0);
-  rect(fruit.x * w, fruit.y*w, w, w)
-    //console.log(pos.y)
-    fill(0, 0, 255);
-  for (i=0; i<tail.length; i++) {
+  rect(fruit.x * w, fruit.y * w, w, w);
+  //console.log(pos.y)
+  fill(0, 0, 255);
+  for (i = 0; i < tail.length; i++) {
     rect(tail[i].x * w, tail[i].y * w, w, w);
   }
 }
