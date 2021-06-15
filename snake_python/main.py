6db73cff6031051a4333ea91841b6bb18eda1c02
misterEarly <afi.lovisfrueh@gmail.com>
2021-06-15 10:41:34 +0200

    refactored code to python indent etc, next is implementing python methods

--- snake_python/main.py
+++ snake_python/main.py
@@ -1,102 +1,84 @@
 import numpy as np
+import random
 
-tiles = 15;
-w = 40;
+tiles = 15
+w = 40
 pos = np.array([7, 7])
 tail = []
-u =  np.array([0, -1])
-d =  np.array([0, -1])
-l =  np.array([0, -1])
-r =  np.array([0, -1])
-let dir;
-let fruit;
+u = np.array([0, -1])
+d = np.array([0, -1])
+l = np.array([0, -1])
+r = np.array([0, -1])
+direct = u
+fruit = np.array([random.randrange(0, tiles), random.randrange(0, tiles)])
 
-function reset() {
-    keyCode = 0;
-tail = [];
-pos = createVector(7, 7);
 
-tail.push(pos);
-for (i = 1; i < 5; i++) {
-    tail.push(createVector(pos.x, pos.y + 1 * i));
-}
-dir = u;
-fruit = createVector(floor(random(tiles)), floor(random(tiles)));
-}
+def reset():
+    for i in range(5):
+        tail.append(np.array([7, 7 + 1 * i]))
 
-function keyPressed() {
+    direct = u;
+    fruit = np.array([random.randrange(0, tiles), random.randrange(0, tiles)])
 
-if (keyIsDown(LEFT_ARROW)) {
-    dir = l;
-keyCode = 0;
-} else if (keyIsDown(RIGHT_ARROW)) {
-dir = r;
-keyCode = 0;
-} else if (keyIsDown(UP_ARROW)) {
-dir = u;
-keyCode = 0;
-} else if (keyIsDown(DOWN_ARROW)) {
-dir = d;
-keyCode = 0;
-}
-}
 
-function setup() {
-    frameRate(5);
+def keyPressed():
+    if (keyIsDown(LEFT_ARROW)):
 
+        dir = l;
+        keyCode = 0;
+    else if (keyIsDown(RIGHT_ARROW)):
+        dir = r;
+        keyCode = 0;
+    else if (keyIsDown(UP_ARROW)):
+        dir = u;
+        keyCode = 0;
+    else if (keyIsDown(DOWN_ARROW)):
+        dir = d;
+        keyCode = 0;
+
+
+frameRate(5);
 createCanvas(tiles * w + 1, tiles * w + 1);
 
-u = createVector(0, -1);
-d = createVector(0, 1);
-l = createVector(-1, 0);
-r = createVector(1, 0);
+reset()
 
-reset();
-}
-
-function draw() {
+while 1:
     background(255);
 
-fill(255);
-strokeWeight(2);
-for (i = 0; i < tiles; i++) {
-for (j = 0; j < tiles; j++) {
-    rect(i * w, j * w, w, w);
-}
-}
+    fill(255);
+    strokeWeight(2);
+    for i in range(tiles):
+        for j in range(tiles):
+            rect(i * w, j * w, w, w);
 
-keyPressed();
+    keyPressed();
 
-tail.splice(0, 0, tail[0].copy().add(dir));
-//console.log(pos.y)
+    tail.splice(0, 0, tail[0].copy().add(dir));
+    // console.log(pos.y)
 
-if (!(tail[0].x == fruit.x && tail[0].y == fruit.y)) {
-tail.splice(tail.length - 1, 1);
-} else {
-fruit = createVector(floor(random(tiles)), floor(random(tiles)));
-}
-for (i = 1; i < tail.length; i++) {
-if (tail[0].x == tail[i].x && tail[0].y == tail[i].y) {
-console.log("dead");
-reset();
-}
-}
+    if (!(tail[0].x == fruit.x & & tail[0].y == fruit.y)):
+        tail.splice(tail.length - 1, 1);
+    else:
+        fruit = createVector(floor(random(tiles)), floor(random(tiles)));
 
-if (tail[0].x < 0) {
-tail[0].x = tiles - 1;
-} else if (tail[0].y < 0) {
-tail[0].y = tiles - 1;
-} else if (tail[0].x > tiles - 1) {
-tail[0].x = 0;
-} else if (tail[0].y > tiles - 1) {
-tail[0].y = 0;
-}
+    for i in range(tail.length):
+        if (tail[0].x == tail[i].x) & & (tail[0].y == tail[i].y):
+            print("dead");
+            reset();
 
-fill(80, 255, 0);
-rect(fruit.x * w, fruit.y * w, w, w);
-//console.log(pos.y)
-fill(0, 0, 255);
-for (i = 0; i < tail.length; i++) {
-rect(tail[i].x * w, tail[i].y * w, w, w);
-}
-}
+    if tail[0][0] < 0:
+        tail[0][0] = tiles - 1
+    elif tail[0][1] < 0:
+        tail[0][1] = tiles - 1
+    elif tail[0][0] > tiles - 1:
+        tail[0][0] = 0
+    elif tail[0][1] > tiles - 1:
+        tail[0][1] = 0
+
+    fill(80, 255, 0);
+    rect(fruit.x * w, fruit.y * w, w, w);
+
+    fill(0, 0, 255);
+    for i in range(tail.length):
+        rect(tail[i].x * w, tail[i].y * w, w, w)
+
