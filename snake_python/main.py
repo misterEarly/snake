3789968e51760d85805db182b2c11fae16a79181
misterEarly <afi.lovisfrueh@gmail.com>
2021-06-15 10:24:57 +0200

    copied js code and started refactoring

--- snake_python/main.py
+++ snake_python/main.py
@@ -1,16 +1,102 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tiles = 15;
+w = 40;
+pos = np.array([7, 7])
+tail = []
+u =  np.array([0, -1])
+d =  np.array([0, -1])
+l =  np.array([0, -1])
+r =  np.array([0, -1])
+let dir;
+let fruit;
 
+function reset() {
+    keyCode = 0;
+tail = [];
+pos = createVector(7, 7);
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+tail.push(pos);
+for (i = 1; i < 5; i++) {
+    tail.push(createVector(pos.x, pos.y + 1 * i));
+}
+dir = u;
+fruit = createVector(floor(random(tiles)), floor(random(tiles)));
+}
 
+function keyPressed() {
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if (keyIsDown(LEFT_ARROW)) {
+    dir = l;
+keyCode = 0;
+} else if (keyIsDown(RIGHT_ARROW)) {
+dir = r;
+keyCode = 0;
+} else if (keyIsDown(UP_ARROW)) {
+dir = u;
+keyCode = 0;
+} else if (keyIsDown(DOWN_ARROW)) {
+dir = d;
+keyCode = 0;
+}
+}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+function setup() {
+    frameRate(5);
+
+createCanvas(tiles * w + 1, tiles * w + 1);
+
+u = createVector(0, -1);
+d = createVector(0, 1);
+l = createVector(-1, 0);
+r = createVector(1, 0);
+
+reset();
+}
+
+function draw() {
+    background(255);
+
+fill(255);
+strokeWeight(2);
+for (i = 0; i < tiles; i++) {
+for (j = 0; j < tiles; j++) {
+    rect(i * w, j * w, w, w);
+}
+}
+
+keyPressed();
+
+tail.splice(0, 0, tail[0].copy().add(dir));
+//console.log(pos.y)
+
+if (!(tail[0].x == fruit.x && tail[0].y == fruit.y)) {
+tail.splice(tail.length - 1, 1);
+} else {
+fruit = createVector(floor(random(tiles)), floor(random(tiles)));
+}
+for (i = 1; i < tail.length; i++) {
+if (tail[0].x == tail[i].x && tail[0].y == tail[i].y) {
+console.log("dead");
+reset();
+}
+}
+
+if (tail[0].x < 0) {
+tail[0].x = tiles - 1;
+} else if (tail[0].y < 0) {
+tail[0].y = tiles - 1;
+} else if (tail[0].x > tiles - 1) {
+tail[0].x = 0;
+} else if (tail[0].y > tiles - 1) {
+tail[0].y = 0;
+}
+
+fill(80, 255, 0);
+rect(fruit.x * w, fruit.y * w, w, w);
+//console.log(pos.y)
+fill(0, 0, 255);
+for (i = 0; i < tail.length; i++) {
+rect(tail[i].x * w, tail[i].y * w, w, w);
+}
+}
